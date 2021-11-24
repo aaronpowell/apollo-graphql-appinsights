1b4147d9d530aae26e64e0424dc8bd5a768b04f3
Aaron Powell <me@aaron-powell.com>
2021-11-24 03:25:46 +0000

    Adding an appinsights logger

--- example/graphql/index.mts
+++ example/graphql/index.mts
@@ -1,5 +1,6 @@
 import { ApolloServer, gql } from "apollo-server-azure-functions";
 import appInsightsPlugin from "@aaronpowell/apollo-server-plugin-appinsights";
+import appInsightsLogger from "@aaronpowell/apollo-server-logger-appinsights";
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -19,6 +20,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [appInsightsPlugin(process.env.APP_INSIGHTS || "")],
+  logger: appInsightsLogger(process.env.APP_INSIGHTS || "graphQL-logger"),
 });
 
 export default server.createHandler();
---
+++ packages/server-logger-appinsights/src/index.ts
@@ -0,0 +1,55 @@
+import { Logger } from "apollo-server-types";
+import { TelemetryClient } from "applicationinsights";
+
+export default function (
+  input: string | TelemetryClient,
+  logName?: string
+): Logger {
+  let client: TelemetryClient;
+  if (typeof input === "string") {
+    client = new TelemetryClient(input);
+  } else {
+    client = input;
+  }
+
+  return {
+    debug(message) {
+      client.trackTrace({
+        message,
+        properties: {
+          logName,
+        },
+      });
+    },
+    error(message) {
+      client.trackException({
+        exception: new Error(message),
+        properties: {
+          logName,
+        },
+      });
+      client.trackTrace({
+        message,
+        properties: {
+          logName,
+        },
+      });
+    },
+    info(message) {
+      client.trackTrace({
+        message,
+        properties: {
+          logName,
+        },
+      });
+    },
+    warn(message) {
+      client.trackTrace({
+        message,
+        properties: {
+          logName,
+        },
+      });
+    },
+  };
+}
